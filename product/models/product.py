df3da5b34677f689c125dcc0e6d62e8d7b08ffb1
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-26 18:04:58 +0100

    fix: add id, fix discount, category and brand

--- product/models/product.py
+++ product/models/product.py
@@ -1,22 +1,25 @@
+from django.utils import timezone
 from django.db import models
 from django.db.models import IntegerField
+from .brand import Brand
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
-    category = models.ManyToManyField('product.Category', related_name='products', default="test_category")
-    brand = models.ManyToManyField('product.Brand', related_name='products', default="test_brand")
+    category = models.ManyToManyField('product.Category', related_name='products', default="test_category", blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products', default="test brand", null=True, blank=True)
 
     size = models.CharField(max_length=20, default="No size specified.")
     description = models.TextField(default="No description available.")
     image = models.ImageField(upload_to='products/')
 
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    discount = IntegerField(max_length=3, default=0)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True)
     discount_end_date = models.DateField(null=True, blank=True)
 
-    stock = models.IntegerField(max_length=5, default=0)
+    stock = models.IntegerField(default=0)
 
 
     def __str__(self):
