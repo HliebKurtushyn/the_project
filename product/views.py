28798a71e4392b38ec1c2866f807dc1960e527c1
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-26 19:36:41 +0100

    feat: add view function for a product details page

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from product.models import Product
+
+
+def product_detail_view(request, id:int):
+    product = get_object_or_404(Product, id=id)
+
+    return render(request, "product/product.html", {"product": product})
