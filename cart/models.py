cfed6ede51182f849d1a902cf02c70c2bed88417
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-30 14:51:25 +0100

    feat: add Cart and CartItem models

---
+++ cart/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+from product.models import Product
+from core.models import CustomUser
+
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='carts')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+
+class CartItem(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+
+    # Ціна буде отримуватись через зв'язок з Product (product.price)
+
+    # Використав MinValueValidator, бо треба щоб мінімальне значення було 1
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Cart {self.cart.id}"
