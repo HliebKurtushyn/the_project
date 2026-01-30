79a50502457cf65907db07c2bc5142954f50aeb7
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-30 18:02:11 +0100

    style: remove unused imports

--- product/views.py
+++ product/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
-from product.models import Product
+from .models import Product
+from cart.models import Cart, CartItem
 
+from cart.utils import get_or_create_cart
 
 def product_detail_view(request, id:int):
     product = get_object_or_404(Product, id=id)
