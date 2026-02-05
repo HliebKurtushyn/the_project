1a8281a8fe1aab469a7c812170bf08332d05880f
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-05 18:23:03 +0100

    feat: load secret key from environment variable for enhanced security

--- the_project/settings.py
+++ the_project/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ijify(u$ye(xmiq-)5m$9wt%k3!yx^c7rc%88f@hz=u-j-*nr0'
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
