3b6d3dd95aba5641b2b526ea552f00fc4c8627e4
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-11 17:10:05 +0100

    fix: rename recently viewed products context variable for clarity

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,4 +6,4 @@ def recently_viewed(request):
     viewed_ids = request.session.get("recently_viewed", [])
     products = Product.objects.filter(id__in=viewed_ids)
 
-    return {"recently_viewed_products": products}
+    return {"recently_viewed": products}
