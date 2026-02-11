f5912c4a23751e3169123d42e6bb86cfd75f26b5
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-11 17:17:54 +0100

    feat: enhance recently viewed products display with links in templates

--- product/views.py
+++ product/views.py
@@ -6,17 +6,17 @@ def product_detail_view(request, id):
     product = get_object_or_404(
         Product.objects.select_related("brand").prefetch_related("category"), id=id
     )
-    recently_viewed = request.session.get("recently_viewed", [])
+    viewed_ids = request.session.get("recently_viewed", [])
 
-    if product.id in recently_viewed:
-        recently_viewed.remove(product.id)
-    recently_viewed.insert(0, product.id)
+    if product.id in viewed_ids:
+        viewed_ids.remove(product.id)
+    viewed_ids.insert(0, product.id)
 
-    request.session["recently_viewed"] = recently_viewed[:5]
+    request.session["recently_viewed"] = viewed_ids[:5]
     request.session.modified = True
 
     return render(
         request,
         "product/product.html",
-        {"product": product, "recently_viewed": recently_viewed},
+        {"product": product},
     )
