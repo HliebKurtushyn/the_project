64d8a353c99e82425594b0c6ad0d048aa0a4f99f
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-15 18:25:35 +0100

    feat: add decorator for caching pages for anonymous users

---
+++ core/cache_utils.py
@@ -0,0 +1,18 @@
+from django.views.decorators.cache import cache_page
+from functools import wraps
+
+
+# Декоратор для кешування сторінок лише для анонімних користувачів
+def cache_page_anonymous(timeout):
+    def _decorator(view_func):
+        cached = cache_page(timeout)(view_func)
+
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                return view_func(request, *args, **kwargs)
+            return cached(request, *args, **kwargs)
+
+        return _wrapped_view
+
+    return _decorator
