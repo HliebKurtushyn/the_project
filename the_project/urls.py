334879c1bd25856ea0e3e6f2608b9558685960b2
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-15 17:35:26 +0100

    feat: add home page with product and category listings

--- the_project/urls.py
+++ the_project/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('product/', include('product.urls')),
-    path('cart/', include('cart.urls')),
-    path('checkout/', include('checkout.urls')),
+    path("", include("home.urls")),
+    path("admin/", admin.site.urls),
+    path("product/", include("product.urls")),
+    path("cart/", include("cart.urls")),
+    path("checkout/", include("checkout.urls")),
 ]
 
 if settings.DEBUG:
