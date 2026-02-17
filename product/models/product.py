6ead2e8809ac2bda0c28d7bb15c0b8cc6e2edee2
HliebKurtushyn <kurtgleb007@gmail.com>
2026-02-17 17:01:26 +0100

    feat: make product image field optional

--- product/models/product.py
+++ product/models/product.py
@@ -6,19 +6,25 @@ class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
-    category = models.ManyToManyField('product.Category', related_name='products', blank=True)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
+    category = models.ManyToManyField(
+        "product.Category", related_name="products", blank=True
+    )
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name="products", null=True, blank=True
+    )
 
     size = models.CharField(max_length=20, default="No size specified.")
     description = models.TextField(default="No description available.")
-    image = models.ImageField(upload_to='products/')
+
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
 
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=True, blank=True)
+    discount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00, null=True, blank=True
+    )
     discount_end_date = models.DateField(null=True, blank=True)
 
     stock = models.IntegerField(default=0)
-
 
     def __str__(self):
         return self.name
